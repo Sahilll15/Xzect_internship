5be70fa68ec3caef3ec1b488cc7e575228e7db74
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-07-15 00:40:34 +0530

    completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
 
   return (
     <nav className={styles.navbar}>
-      <div className="logo">Logo</div>
+      <div className={styles.logo}>Xzent</div>
       <div className={`${styles.navbaritems} ${isOpen ? 'open' : ''}`}>
        {
         isOpen &&(
